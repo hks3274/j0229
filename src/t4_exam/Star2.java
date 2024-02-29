729c737f9b9ba059a101bf3ac33be27c72692e45
sooYeon <hks3274@naver.com>
2024-02-29 18:03:52 +0900

    20240229_18:03

---
+++ src/t4_exam/Star2.java
@@ -0,0 +1,24 @@
+package t4_exam;
+/*
+  1 4 7
+  2 5 8
+  3 6 9
+ */
+
+import java.util.Scanner;
+
+public class Star2 {
+	public static void main(String[] args) {
+		
+		int su = -2;
+		
+		for (int i = 1; i <= 3; i++) {
+			for (int j = 1; j <= 3; j++) {
+				su += 3 ;
+				System.out.print(su+" ");
+			}
+			su -= 8;
+			System.out.println();
+		}
+	}
+}
